0f6f7553071964b1fa757a48d696da0408e3cbe5
Amir Berenji <a.berenji62@gmail.com>
2024-10-05 16:36:08 +0400

    Create Tomorrow and day after tomorrow task widgets

---
+++ lib/features/todo/widgets/tomorrow_list.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:todo_proj/common/utils/constants.dart';
+import 'package:todo_proj/common/widgets/xpansion_tile.dart';
+import 'package:todo_proj/features/todo/controllers/todo/todo_provider.dart';
+import 'package:todo_proj/features/todo/controllers/xpansion_provider.dart';
+import 'package:todo_proj/features/todo/widgets/todo_tile.dart';
+
+class TomorrowList extends ConsumerWidget {
+  const TomorrowList({super.key});
+
+  @override
+  Widget build(BuildContext context,WidgetRef ref) {
+    final todos = ref.watch(todoStateProvider);
+    var color = ref.read(todoStateProvider.notifier).getRandomColor();
+    String tomorrow = ref.read(todoStateProvider.notifier).getTomorrow();
+    var tomorrowsTasks = todos.where((e)=>e.date!.contains(tomorrow)).toList();
+
+    return XpansionTile(
+                text: "Tomorrow's Task", 
+                text2: "Tomorrow's tasks are shown here", 
+                onExpansionChanged: (bool expanded){ 
+                  ref.read(xpansionStateProvider.notifier)
+                  .setStart(!expanded);
+                },
+                trailing: Padding(
+                  padding: EdgeInsets.only(right: 12.w),
+                  child: ref.watch(xpansionStateProvider)
+                  ? const Icon(AntDesign.circledown,color: AppConst.kLight ,)
+                  : const Icon(AntDesign.closecircleo,color: AppConst.kBlueLight),
+                ),
+                children: [
+                  for(final todo in tomorrowsTasks)
+                  TodoTile(
+                    title: todo.title,
+                    description: todo.desc,
+                    color: color,
+                    start: todo.startTime,
+                    end: todo.endTime,
+                    delete: (){
+                          ref.read(todoStateProvider.notifier).deleteTodo(todo.id??0);
+                        },
+                        editWidget: GestureDetector(onTap: () {},
+                        child: const Icon(MaterialCommunityIcons.circle_edit_outline),
+                        ),
+                    switcher: const SizedBox.shrink() 
+                    ),
+                ]);
+  }
+}
